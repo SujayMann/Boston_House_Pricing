594113b628e8fae33661accc22cdfd24fc0986e2
Sujay Mann <sujay2004mann@gmail.com>
2024-03-22 18:27:07 +0530

    css update

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import pickle
-from flask import Flask, request, app, jsonify, url_for, render_template
+from flask import Flask, request, app, jsonify, render_template
 import numpy as np
 import pandas as pd
 
