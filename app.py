b9f93505ac4ae4c081a2411e674236c140416bf6
Sujay Mann <sujay2004mann@gmail.com>
2024-03-28 14:22:20 +0530

    model update

--- app.py
+++ app.py
@@ -5,8 +5,10 @@ import pandas as pd
 
 app = Flask(__name__)
 # Load the model
-regmodel = pickle.load(open("regmodel.pkl", "rb"))
-scaler = pickle.load(open("scaling.pkl", "rb"))
+# regmodel = pickle.load(open("regmodel.pkl", "rb"))
+# scaler = pickle.load(open("scaling.pkl", "rb"))
+model = pickle.load(open("model.pkl", "rb"))
+scaler = pickle.load(open("scaler.pkl", "rb"))
 
 @app.route('/')
 def home():
@@ -18,7 +20,7 @@ def predict_api():
     print(data)
     print(np.array(list(data.values())).reshape(1, -1))
     new_data = scaler.transform(np.array(list(data.values())).reshape(1, -1))
-    output = regmodel.predict(new_data)
+    output = model.predict(new_data)
     print(output[0])
     return jsonify(output[0])
 
@@ -27,7 +29,7 @@ def predict():
     data = [float(x) for x in request.form.values()]
     final_input = scaler.transform(np.array(data).reshape(1, -1))
     print(final_input)
-    output = float(regmodel.predict(final_input)[0])
+    output = float(model.predict(final_input)[0])
     return render_template('home.html', prediction_text="The predicted house price is {:.3f} ($1000s)".format(output))
 
 if __name__ == "__main__":
