ef315a5c52fbe405f81b338e5a94554eae5f280e
Sujay Mann <sujay2004mann@gmail.com>
2024-03-29 10:17:30 +0530

    version update

--- app.py
+++ app.py
@@ -14,15 +14,15 @@ scaler = pickle.load(open("scaler.pkl", "rb"))
 def home():
     return render_template('home.html')
 
-@app.route('/predict_api', methods=['POST'])
-def predict_api():
-    data = request.json['data']
-    print(data)
-    print(np.array(list(data.values())).reshape(1, -1))
-    new_data = scaler.transform(np.array(list(data.values())).reshape(1, -1))
-    output = tree_model.predict(new_data)
-    print(output[0])
-    return jsonify(output[0])
+# @app.route('/predict_api', methods=['POST'])
+# def predict_api():
+#     data = request.json['data']
+#     print(data)
+#     print(np.array(list(data.values())).reshape(1, -1))
+#     new_data = scaler.transform(np.array(list(data.values())).reshape(1, -1))
+#     output = tree_model.predict(new_data)
+#     print(output[0])
+#     return jsonify(output[0])
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -30,7 +30,7 @@ def predict():
     print(data, "hello")
     final_input = scaler.transform(np.array(data).reshape(1, -1))
     print(final_input)
-    output = float(tree_model.predict(final_input))
+    output = float(tree_model.predict(final_input)[0])
     return render_template('home.html', prediction_text="The predicted house price is {:.3f} ($1000s)".format(output))
 
 if __name__ == "__main__":
