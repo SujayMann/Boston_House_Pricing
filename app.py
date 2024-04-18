306770bdffa478c258a0a2f0afd238abbfa99cb7
Sujay Mann <sujay2004mann@gmail.com>
2024-04-18 20:07:22 +0530

    'model_update'

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 app = Flask(__name__)
 # Load the model
-tree_model = pickle.load(open("model.pkl", "rb"))
+model = pickle.load(open("model.pkl", "rb"))
 scaler = pickle.load(open("scaler.pkl", "rb"))
 
 @app.route('/')
@@ -18,7 +18,7 @@ def predict():
     print(data, "hello")
     final_input = scaler.transform(np.array(data).reshape(1, -1))
     print(final_input)
-    output = float(tree_model.predict(final_input)[0])
+    output = float(model.predict(final_input)[0])
     return render_template('home.html', prediction_text="The predicted house price is {:.3f} ($1000s)".format(output))
 
 if __name__ == "__main__":
