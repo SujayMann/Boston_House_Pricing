dc82378937416b5d51b994880cf3f5e9860071f3
Sujay Mann <sujay2004mann@gmail.com>
2024-04-18 10:25:35 +0530

    'commit'

--- app.py
+++ app.py
@@ -5,24 +5,12 @@ import pandas as pd
 
 app = Flask(__name__)
 # Load the model
-# regmodel = pickle.load(open("regmodel.pkl", "rb"))
-# scaler = pickle.load(open("scaling.pkl", "rb"))
 tree_model = pickle.load(open("model.pkl", "rb"))
 scaler = pickle.load(open("scaler.pkl", "rb"))
 
 @app.route('/')
 def home():
     return render_template('home.html')
-
-# @app.route('/predict_api', methods=['POST'])
-# def predict_api():
-#     data = request.json['data']
-#     print(data)
-#     print(np.array(list(data.values())).reshape(1, -1))
-#     new_data = scaler.transform(np.array(list(data.values())).reshape(1, -1))
-#     output = tree_model.predict(new_data)
-#     print(output[0])
-#     return jsonify(output[0])
 
 @app.route('/predict', methods=['POST'])
 def predict():
